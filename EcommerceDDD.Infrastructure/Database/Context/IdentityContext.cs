84e5aeb090fea385d09ce5ab4c4514746bdab7f7
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:40:34 -0400

    Added database infrastructure

---
+++ EcommerceDDD.Infrastructure/Database/Context/IdentityContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using EcommerceDDD.Infrastructure.Identity.IdentityUser;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDDD.Infrastructure.Database.Context
+{
+    public class IdentityContext : IdentityDbContext<User, Role, Guid>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(b =>
+            {
+                b.Property(u => u.Id).HasDefaultValueSql("newsequentialid()");
+            });
+
+            modelBuilder.Entity<Role>(b =>
+            {
+                b.Property(u => u.Id).HasDefaultValueSql("newsequentialid()");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EcommerceDDD.Infrastructure/Database/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace EcommerceDDD.Infrastructure.Database.Extensions
+{
+	public static class IQueryableExtensions
+	{
+        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
+        {
+            var enumerator = query.Provider.Execute<IEnumerable<TEntity>>(query.Expression).GetEnumerator();
+            var relationalCommandCache = enumerator.Private("_relationalCommandCache");
+            var selectExpression = relationalCommandCache.Private<SelectExpression>("_selectExpression");
+            var factory = relationalCommandCache.Private<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
+
+            var sqlGenerator = factory.Create();
+            var command = sqlGenerator.GetCommand(selectExpression);
+
+            string sql = command.CommandText;
+            return sql;
+        }
+
+        private static object Private(this object obj, string privateField) => obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+        private static T Private<T>(this object obj, string privateField) => (T)obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+    }
+}
