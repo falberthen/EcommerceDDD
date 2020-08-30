65fa3c5844f79bf3c6d23d70316ef845d48702ac
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:46:36 -0400

    Added application event sourcing helpers

---
+++ EcommerceDDD.Application/EventSourcing/EventHistoryData/CustomerHistoryData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcommerceDDD.Application.EventSourcing.EventHistoryData
+{
+    public class CustomerHistoryData : HistoryData
+    {
+        public string Name { get; set; }
+    }
+}
