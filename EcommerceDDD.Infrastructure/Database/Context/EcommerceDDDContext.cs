a9de1bf229eb1a0e88c69ed3b0cd77edd1c3a525
Felipe Henrique <fealberto@gmail.com>
2020-08-30 01:40:34 -0400

    Added database infrastructure

---
+++ EcommerceDDD.Infrastructure/Database/Context/EcommerceDDDContext.cs
@@ -0,0 +1,32 @@
+﻿using EcommerceDDD.Domain.Core.Messaging;
+using EcommerceDDD.Domain.Customers;
+using EcommerceDDD.Domain.Customers.Orders;
+using EcommerceDDD.Domain.Products;
+using EcommerceDDD.Infrastructure.Database.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDDD.Infrastructure.Database.Context
+{
+    public sealed class EcommerceDDDContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StoredEvent> StoredEvents { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+
+        public EcommerceDDDContext(DbContextOptions<EcommerceDDDContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<Event>();
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new StoredMessageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
