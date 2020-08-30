d3626cae587b5e8e91ba8fef73f45297df095c9f
Felipe Henrique <fealberto@gmail.com>
2020-08-30 01:44:24 -0400

    Added messaging infrastructure
    Added dataseed project

---
+++ EcommerceDDD.DataSeed/SeedProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EcommerceDDD.Domain;
+using EcommerceDDD.Domain.CurrencyExchange;
+using EcommerceDDD.Domain.Products;
+using EcommerceDDD.Domain.Shared;
+
+namespace EcommerceDDD.DataSeed
+{
+    public static class SeedProducts
+    {
+        public async static Task SeedData(IEcommerceUnitOfWork unitOfWork, ICurrencyConverter converter)
+        {
+            //Creating products
+            List<Product> products = new List<Product>();
+            var rand = new Random();
+
+            for (int i = 0; i < 50; i++)
+            {
+                var price = new decimal(rand.NextDouble());
+                products.Add(new Product($"Product {i}", Money.Of(price, converter.GetBaseCurrency().Name)));
+            }
+
+            await unitOfWork.ProductRepository.AddProducts(products);
+            await unitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ EcommerceDDD.Infrastructure/Messaging/MessagePublisher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EcommerceDDD.Domain;
+using EcommerceDDD.Domain.Core.Base;
+using EcommerceDDD.Domain.Core.Messaging;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace EcommerceDDD.Infrastructure.Messaging
+{
+    public class MessagePublisher : IMessagePublisher
+    {
+        private readonly ILogger<MessagePublisher> _logger;
+        private readonly IMediator _mediator;
+
+        public MessagePublisher(ILogger<MessagePublisher> logger, IMediator mediator)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mediator = mediator;
+        }
+
+        public Task Publish(StoredEvent message, System.Threading.CancellationToken cancellationToken)
+        {
+            Type messageType = GetType(message.MessageType);
+            var domainEvent = JsonConvert.DeserializeObject(message.Payload, messageType);
+
+            if (messageType != null 
+                && domainEvent != null)
+            {
+                _mediator.Publish(domainEvent);
+                _logger.LogInformation($"message {message.Id} processed!");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = a.GetType(typeName);
+                if (type != null)
+                    return type;
+            }
+            return null;
+        }
+    }
+}
