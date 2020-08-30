34ade0d29a814b4a562b186924804d1e0c46e43c
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:44:24 -0400

    Added messaging infrastructure

---
+++ EcommerceDDD.Infrastructure/Messaging/IEventSerializer.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceDDD.Domain.Core.Messaging;
+
+namespace EcommerceDDD.Infrastructure.Messaging
+{
+    public interface IEventSerializer
+    {
+        string Serialize<TE>(TE @event) where TE : Event;
+    }
+}
---
+++ EcommerceDDD.Infrastructure/Messaging/IMessagePublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EcommerceDDD.Domain.Core.Messaging;
+
+namespace EcommerceDDD.Infrastructure.Messaging
+{
+    public interface IMessagePublisher
+    {
+        Task Publish(StoredEvent message, System.Threading.CancellationToken cancellationToken);
+    }
+}
