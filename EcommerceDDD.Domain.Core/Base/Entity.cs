97033cff597ba5bbdc10d417e6d76ae676079d7b
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:30:38 -0400

    Added domain building blocks

---
+++ EcommerceDDD.Domain.Core/Base/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using EcommerceDDD.Domain.Core.Messaging;
+
+namespace EcommerceDDD.Domain.Core.Base
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public IReadOnlyCollection<Event> DomainEvents => _domainEvents?.AsReadOnly();
+
+        public void AddDomainEvent<TE>(TE @event) where TE : Event
+        {
+            _domainEvents = _domainEvents ?? new List<Event>();
+            _domainEvents.Add(@event);
+        }
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        private List<Event> _domainEvents;
+    }
+}
---
+++ EcommerceDDD.Domain.Core/Base/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EcommerceDDD.Domain.Core.Base
+{
+    public interface IRepository<T> where T : IAggregateRoot
+    {
+    }
+}
