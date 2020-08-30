3fe9f5c7a6828d4be11465e516058395d2db846d
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:34:40 -0400

    Added domain customer orders

---
+++ EcommerceDDD.Domain/Customers/Orders/Basket.cs
@@ -0,0 +1,48 @@
+﻿using EcommerceDDD.Domain.Core.Base;
+using EcommerceDDD.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EcommerceDDD.Domain.Customers.Orders
+{
+    public class Basket
+    {
+        public string Currency { get; private set; }
+        public List<BasketProduct> Products { get; private set; } = new List<BasketProduct>();
+
+        public Basket(string currency)
+        {
+            if (string.IsNullOrEmpty(currency))
+                throw new BusinessRuleException("The used currency must be informed.");
+
+            Currency = currency;
+        }
+
+        public void AddProduct(Guid productId, Money price, int quantity)
+        {
+            if (productId == null)
+                throw new ArgumentNullException(nameof(productId));
+
+            if (price == null)
+                throw new ArgumentNullException(nameof(price));
+      
+            Products.Add(new BasketProduct(productId, price, quantity));
+        }
+
+        public class BasketProduct
+        {
+            public Guid ProductId { get; private set; }
+            public Money Price { get; private set; }
+            public int Quantity { get; private set; }
+
+            public BasketProduct(Guid productId, Money price, int quantity)
+            {
+                ProductId = productId;
+                Price = price;
+                Quantity = quantity;
+            }
+        }
+    }
+}
---
+++ EcommerceDDD.Domain/Customers/Orders/Order.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EcommerceDDD.Domain.Core.Base;
+using EcommerceDDD.Domain.CurrencyExchange;
+using EcommerceDDD.Domain.Shared;
+
+namespace EcommerceDDD.Domain.Customers.Orders
+{
+    public class Order : Entity
+    {
+        public OrderStatus Status { get; private set; }
+        public DateTime OrderDate { get; private set; }
+        public DateTime? ChangeDate { get; private set; }
+        public bool IsCancelled { get; private set; }
+        public Money TotalPrice { get; private set; }
+        public List<OrderLine> OrderLines { get; private set; } = new List<OrderLine>();        
+
+        public Order(Basket basket, ICurrencyConverter converter)
+        {
+            OrderDate = DateTime.Now;            
+            Status = OrderStatus.Placed;
+            BuildOrderLines(basket, converter);
+            CalculateTotalPrice(basket.Currency);
+        }
+
+        public void Change(Basket basket, ICurrencyConverter converter)
+        {
+            ChangeDate = DateTime.UtcNow;
+            RemoveOrderLine(basket);
+            BuildOrderLines(basket, converter);
+            CalculateTotalPrice(basket.Currency);
+        }
+
+        private void BuildOrderLines(Basket basket, ICurrencyConverter converter)
+        {
+            foreach (var product in basket.Products)
+            {
+                var existingOrderLine = OrderLines.FirstOrDefault(o => o.ProductId == product.ProductId);
+                if(existingOrderLine == null)
+                {
+                    OrderLines.Add(new OrderLine(
+                        Id,
+                        product.ProductId,
+                        product.Price,
+                        product.Quantity,
+                        basket.Currency,
+                        converter)
+                    );
+                }
+                else
+                {
+                    var basketProduct = basket.Products.Single(p => p.ProductId == product.ProductId);
+                    existingOrderLine.ChangeQuantity(basketProduct.Quantity, product.Price, basket.Currency, converter);
+                }
+            }
+        }
+
+        private void RemoveOrderLine(Basket cart)
+        {
+            var orderLines = OrderLines.ToList();
+            foreach (var orderline in orderLines)
+            {
+                var product = cart.Products.SingleOrDefault(x => x.ProductId == orderline.ProductId);
+                if (product == null)
+                    OrderLines.Remove(orderline);
+            }
+        }
+
+        private void CalculateTotalPrice(string currency)
+        {
+            var total = OrderLines.Sum(x => x.ProductExchangePrice.Value);
+            TotalPrice = Money.Of(total, currency);
+        }
+
+        // Empty constructor for EF
+        private Order() { }
+    }
+}
