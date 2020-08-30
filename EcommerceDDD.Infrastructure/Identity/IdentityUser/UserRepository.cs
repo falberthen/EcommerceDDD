2434d31107c3f3fa6140d6f37474affb56fd2019
Felipe Henrique <fealberto@gmail.com>
2020-08-30 01:38:59 -0400

    Added identity infrastructure

---
+++ EcommerceDDD.Infrastructure/Identity/IdentityUser/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceDDD.Infrastructure.Database.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceDDD.Infrastructure.Identity.IdentityUser
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IdentityContext _dbContext;
+
+        public UserRepository(IdentityContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(c => c.Email == email);
+        }
+    }
+}
---
+++ EcommerceDDD.Infrastructure/Identity/Services/IJwtService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDDD.Infrastructure.Identity.Services
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateJwt(string email);
+    }
+}
