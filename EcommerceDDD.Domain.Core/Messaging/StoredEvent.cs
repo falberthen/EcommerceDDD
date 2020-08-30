c67b865997a5604ddd24b7d72b52f6356f25585e
Felipe Alberto <fealberto@gmail.com>
2020-08-30 01:31:14 -0400

    Added domain messaging

---
+++ EcommerceDDD.Domain.Core/Messaging/StoredEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDDD.Domain.Core.Messaging
+{
+    public class StoredEvent : Event
+    {
+        public StoredEvent(Event @event, string payload)
+        {
+            Id = Guid.NewGuid();
+            AggregateId = @event.AggregateId;
+            MessageType = @event.MessageType;
+            Payload = payload;
+        }
+
+        public void SetProcessedAt(DateTime date)
+        {
+            if (date == null)
+                throw new ArgumentNullException(nameof(date));
+            ProcessedAt = date;
+        }
+
+        // EF Constructor
+        protected StoredEvent() { }
+
+        public Guid Id { get; private set; }
+        public string Payload { get; private set; }
+        public DateTime? ProcessedAt { get; private set; }       
+    }
+}
