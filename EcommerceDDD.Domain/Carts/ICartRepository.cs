e3c24793e96fea6f51069f91c98639a54ecefe31
Felipe Alberto <fealberto@gmail.com>
2020-09-22 22:04:02 -0400

    Added cart

---
+++ EcommerceDDD.Domain/Carts/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EcommerceDDD.Domain.Core.Base;
+
+namespace EcommerceDDD.Domain.Carts
+{
+    public interface ICartRepository : IRepository<Cart>
+    {
+        Task Add(Cart cart, CancellationToken cancellationToken = default);
+        Task<Cart> GetById(Guid cartId, CancellationToken cancellationToken = default);
+    }
+}
