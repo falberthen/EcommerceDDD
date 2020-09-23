2051071b114b8e599cd154494bb771b2e43e6d79
Felipe Alberto <fealberto@gmail.com>
2020-09-22 22:04:02 -0400

    Added cart

---
+++ EcommerceDDD.Domain/Carts/CartItem.cs
@@ -0,0 +1,33 @@
+﻿using EcommerceDDD.Domain.Core.Base;
+using EcommerceDDD.Domain.Products;
+using EcommerceDDD.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EcommerceDDD.Domain.Carts
+{
+    public class CartItem : Entity
+    {
+        public Product Product { get; private set; }
+        public int Quantity { get; private set; }
+
+        public CartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public void ChangeQuantity(int quantity)
+        {
+            if (quantity == 0)
+                throw new BusinessRuleException("The product quantity must be at last 1.");
+
+            Quantity = quantity;
+        }
+
+        // Empty constructor for EF
+        private CartItem() { }
+    }
+}
