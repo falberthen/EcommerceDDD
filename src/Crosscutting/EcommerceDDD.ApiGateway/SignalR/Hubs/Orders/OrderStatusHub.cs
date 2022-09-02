e9acc6611912e79ad3976bdaae51dcc7769b0787
Felipe Alberto <fealberto@gmail.com>
2022-09-01 22:36:48 -0400

    Added EcommerceDDD.ApiGateway

---
+++ src/Crosscutting/EcommerceDDD.ApiGateway/SignalR/Hubs/Orders/OrderStatusHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace EcommerceDDD.ApiGateway.SignalR.Hubs.Order;
+
+public interface IOrderStatusHubClient
+{
+    Task UpdateOrderStatus(string orderId, string orderStatusText, int orderStatusCode);
+}
+
+public class OrderStatusHub : Hub<IOrderStatusHubClient>
+{
+    public async Task JoinCustomerToGroup(string customerId) =>    
+        await Groups.AddToGroupAsync(Context.ConnectionId, customerId);
+
+}
