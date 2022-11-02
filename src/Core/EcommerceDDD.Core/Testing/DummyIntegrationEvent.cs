a75905f6821e1d2a61cd4366e13ff35844ff79c4
Felipe Alberto <fealberto@gmail.com>
2022-11-02 15:05:23 -0400

    Added EventDispatcher based on INotification

---
+++ src/Core/EcommerceDDD.Core/Testing/DummyIntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿using EcommerceDDD.Core.EventBus;
+
+namespace EcommerceDDD.Core.Testing;
+
+public sealed class DummyIntegrationEvent : IIntegrationEvent
+{
+    public Guid Id { get; set; }
+    public string Text { get; set; }
+}
