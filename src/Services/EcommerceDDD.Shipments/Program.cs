5a52fdd10a78c58362aed06029236d057c925533
Felipe Alberto <fealberto@gmail.com>
2022-11-07 20:26:45 -0500

    Added OutboxSettings / extension

--- src/Services/EcommerceDDD.Shipments/Program.cs
+++ src/Services/EcommerceDDD.Shipments/Program.cs
@@ -1,15 +1,15 @@
 using EcommerceDDD.Core.Testing;
 using EcommerceDDD.Core.Persistence;
 using EcommerceDDD.Shipments.Domain;
+using EcommerceDDD.Core.Infrastructure.Kafka;
+using EcommerceDDD.Core.Infrastructure.Outbox;
 using EcommerceDDD.Core.Infrastructure.Marten;
 using EcommerceDDD.Core.Infrastructure.WebApi;
 using EcommerceDDD.Core.Infrastructure.Identity;
-using EcommerceDDD.Core.Infrastructure.Kafka;
+using EcommerceDDD.Core.Infrastructure.EventBus;
 using EcommerceDDD.Core.Infrastructure.Integration;
 using EcommerceDDD.Shipments.Infrastructure.Projections;
 using EcommerceDDD.Shipments.Application.ShippingPackage;
-using EcommerceDDD.Core.Infrastructure.Outbox;
-using EcommerceDDD.Core.Infrastructure.EventBus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,10 +28,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAuthentication(builder.Configuration);
 builder.Services.AddSwagger(builder.Configuration);
-builder.Services.AddOutboxSetup(builder.Configuration);
 builder.Services.AddEventDispatcher();
 builder.Services.AddMarten(builder.Configuration, options =>
     options.ConfigureProjections());
+
+// ---- Outbox
+builder.Services.AddOutboxPollingService(builder); // Comment to stop polling the database
+//builder.Services.AddOutboxDebeziumConnector(builder); Uncomment to use Debezium
 
 // ---- App
 var app = builder.Build();
@@ -43,4 +46,4 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-app.Run();
+app.EnsureDatabaseCreated(builder).Run();
