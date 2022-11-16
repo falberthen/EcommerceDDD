abdd20fe8ae43ddb4f7fd1fbe5e7299aa0ed3686
Felipe Henrique <fealberto@gmail.com>
2022-11-16 10:08:35 -0500

    Refactored outbox implementation

--- src/Core/EcommerceDDD.Core.Infrastructure/Outbox/Services/IOutboxMessageService.cs
+++ src/Core/EcommerceDDD.Core.Infrastructure/Outbox/Services/IOutboxMessageService.cs
@@ -1,8 +1,8 @@
-﻿namespace EcommerceDDD.Core.Infrastructure.Outbox.Services;
+﻿using EcommerceDDD.Core.EventBus;
+
+namespace EcommerceDDD.Core.Infrastructure.Outbox.Services;
 
 public interface IOutboxMessageService
 {
-    public Task<OutboxMessage> SaveAsOutboxMessageAsync(INotification @event);
-    public Task<IEnumerable<OutboxMessage>> FetchUnprocessedMessagesAsync(int batchSize, CancellationToken cancellationToken);
-    public Task ProcessMessageAsync(OutboxMessage message, CancellationToken cancellationToken);
+    public Task SaveAsOutboxMessageAsync(IntegrationEvent @event, bool saveChanges = false);
 }
