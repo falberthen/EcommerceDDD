ec5001050c8b702051f0040a2f759dc1dffd7cdc
Felipe Henrique <fealberto@gmail.com>
2026-02-24 20:59:26 -0500

    feat(otel): add ActivitySources constants and register Kafka consumer source

--- src/Core/EcommerceDDD.Core.Infrastructure/OpenTelemetry/OpenTelemetryExtension.cs
+++ src/Core/EcommerceDDD.Core.Infrastructure/OpenTelemetry/OpenTelemetryExtension.cs
@@ -12,6 +12,7 @@ public static class OpenTelemetryExtension
                 tracing
                     .AddAspNetCoreInstrumentation()
                     .AddHttpClientInstrumentation()
+                    .AddSource(ActivitySources.KafkaConsumer)
                     .AddOtlpExporter();
             })
             .WithMetrics(metrics =>
