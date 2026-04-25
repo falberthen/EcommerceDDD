cb9fffcaea75e0e4528eeb2773b9fc4ded40c0cd
Felipe Henrique <fealberto@gmail.com>
2026-04-25 16:20:53 -0400

    fix(payment-client): correct CancelPaymentAsync parameter order and regenerate Kiota client

--- src/Crosscutting/EcommerceDDD.ServiceClients/Services/Payment/IPaymentService.cs
+++ src/Crosscutting/EcommerceDDD.ServiceClients/Services/Payment/IPaymentService.cs
@@ -3,5 +3,5 @@ namespace EcommerceDDD.ServiceClients.Services.Payment;
 public interface IPaymentService
 {
     Task RequestPaymentAsync(Guid customerId, Guid orderId, string currencyCode, double totalAmount, IList<PaymentProductItem> items, CancellationToken cancellationToken);
-    Task CancelPaymentAsync(Guid paymentId, int cancellationReason, CancellationToken cancellationToken);
+    Task CancelPaymentAsync(Guid orderId, Guid paymentId, int cancellationReason, CancellationToken cancellationToken);
 }
--- src/Crosscutting/EcommerceDDD.ServiceClients/Services/Shipment/ShipmentService.cs
+++ src/Crosscutting/EcommerceDDD.ServiceClients/Services/Shipment/ShipmentService.cs
@@ -1,29 +1,27 @@
-using EcommerceDDD.ServiceClients.ShipmentProcessing;
-using EcommerceDDD.ServiceClients.ShipmentProcessing.Models;
 
 namespace EcommerceDDD.ServiceClients.Services.Shipment;
 
 public class ShipmentService(ShipmentProcessingClient shipmentProcessingClient) : IShipmentService
 {
-    private readonly ShipmentProcessingClient _shipmentProcessingClient = shipmentProcessingClient;
+	private readonly ShipmentProcessingClient _shipmentProcessingClient = shipmentProcessingClient;
 
-    public async Task RequestShipmentAsync(Guid orderId, IList<ShipmentProductItem> items, CancellationToken cancellationToken)
-    {
-        var productItems = items.Select(i => new ProductItemRequest()
-        {
-            ProductId = i.ProductId,
-            ProductName = i.ProductName,
-            Quantity = i.Quantity,
-            UnitPrice = i.UnitPrice
-        }).ToList();
+	public async Task RequestShipmentAsync(Guid orderId, IList<ShipmentProductItem> items, CancellationToken cancellationToken)
+	{
+		var productItems = items.Select(i => new ShipmentProcessing.Models.ProductItemRequest()
+		{
+			ProductId = i.ProductId,
+			ProductName = i.ProductName,
+			Quantity = i.Quantity,
+			UnitPrice = i.UnitPrice
+		}).ToList();
 
-        var request = new ShipOrderRequest()
-        {
-            OrderId = orderId,
-            ProductItems = productItems
-        };
+		var request = new ShipOrderRequest()
+		{
+			OrderId = orderId,
+			ProductItems = productItems
+		};
 
-        await _shipmentProcessingClient.Api.V2.Internal.Shipments
-            .PostAsync(request, cancellationToken: cancellationToken);
-    }
+		await _shipmentProcessingClient.Api.V2.Internal.Shipments
+			.PostAsync(request, cancellationToken: cancellationToken);
+	}
 }
